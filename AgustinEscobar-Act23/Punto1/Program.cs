1eab54282407a52b917ea18c9500a142c98fdb7a
Agustin-Escobar978 <agustinescobar978@gmail.com>
2026-08-06 10:05:45 -0300

    avances de la actividad 23

---
+++ AgustinEscobar-Act23/Punto1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    class Documento
+    {
+        private string nombreArchivo;
+        private int cantidadPaginas;
+
+        public string NombreArchivo
+        {
+            get {  return nombreArchivo; }
+        }
+        public int CantidadPaginas
+        {
+            get { return cantidadPaginas; }
+        }
+
+        public Documento(string nom, int pag)
+        {
+            nombreArchivo = nom;
+            cantidadPaginas = pag;
+        }
+    }
+
+    class ServidorImpresion
+    {
+        List <Documento> documentos = new List<Documento> ();
+
+        public void agregarDocumento()
+        {
+            string nombre;
+            int paginas;
+            Console.Write("Ingrese el nombre de archivo del documento: ");
+            nombre = Console.ReadLine ();
+            Console.Write("Ingrese la cantidad de paginas de un documento: ");
+            paginas = int.Parse(Console.ReadLine());
+            Documento nuevoDocumento = new Documento(nombre, paginas);
+            documentos.Add(nuevoDocumento);
+            Console.WriteLine();
+        }
+
+        public void imprimirSiguiente()
+        {
+            if (documentos.Count == 0)
+            {
+                Console.WriteLine("No hay trabajos pendientes\n");
+            }
+            else
+            {
+                Console.WriteLine($"Nombre archivo: {documentos[0].NombreArchivo}\nCantidad de paginas: {documentos[0].CantidadPaginas}\n");
+                documentos.RemoveAt(0);
+            }
+        }
+
+        public void mostrarColaPendiente()
+        {
+            int paginasTotales = 0;
+            foreach(Documento doc in documentos)
+            {
+                Console.WriteLine($"- {doc.NombreArchivo}");
+                paginasTotales += doc.CantidadPaginas;
+            }
+            Console.WriteLine($"La cantidad total de paginas acumuladas en la cola son: {paginasTotales}");
+        }
+    }
+
+    class Cargar
+    {
+        static void Main(string[] args)
+        {
+            ServidorImpresion si1 = new ServidorImpresion();
+            si1.imprimirSiguiente();
+
+            si1.agregarDocumento();
+            si1.imprimirSiguiente();
+            si1.imprimirSiguiente();
+
+            si1.agregarDocumento();
+            si1.agregarDocumento();
+            si1.agregarDocumento();
+            si1.mostrarColaPendiente();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AgustinEscobar-Act23/Punto2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    class Dron
+    {
+        private string codigo;
+        private int nivelBateria;
+
+        public string Codigo
+        {
+            set { codigo = value; }
+            get { return codigo; }
+        }
+        public int NivelBateria
+        {
+            set
+            {
+                if (value >= 0 && value <= 100 )
+                {
+                    nivelBateria = value;
+                }
+                else
+                {
+                    bool ciclo = true;
+                    do
+                    {
+                        Console.Write("Ingrese un nivel de bateria valido (0 al 100): ");
+                        int nuevaBateria = int.Parse(Console.ReadLine());
+                        if (nuevaBateria >= 0 && nuevaBateria <= 100)
+                        {
+                            nivelBateria = nuevaBateria;
+                            ciclo = false;
+                        }
+                    } while (ciclo == true);
+                    Console.WriteLine();
+                }
+            }
+            get { return nivelBateria;  }
+        }
+
+        public Dron(string cod, int bat)
+        {
+            Codigo = cod;
+            NivelBateria = bat;
+        }
+    }
+
+    class CentroControl
+    {
+        List<Dron> drones = new List<Dron>();
+
+        public CentroControl()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Ingrese el codigo del {i + 1}° dron: ");
+                string cod = Console.ReadLine();
+                Console.Write($"Ingrese el nivel de bateria del {i + 1}° dron: ");
+                int bat = int.Parse(Console.ReadLine());
+                Dron nuevoDron = new Dron(cod, bat);
+                drones.Add(nuevoDron);
+                Console.WriteLine();
+            }
+        }
+
+        public void listarFlota()
+        {
+            foreach(Dron dron in drones)
+            {
+                Console.WriteLine($"- El dron {dron.Codigo} tiene {dron.NivelBateria}% bateria");
+            }
+            Console.WriteLine();
+        }
+
+        public void removerDronesBajos()
+        {
+            drones.RemoveAll(n => n.NivelBateria <= 15);
+            Console.WriteLine($"Todos los drones con bateria menor a 16% fueron eliminados");
+        }
+
+        public void mostrarDronesRestantes()
+        {
+            Console.WriteLine("***Flota actualizada***");
+            foreach(Dron dron in drones)
+            {
+                Console.WriteLine($"- El dron {dron.Codigo} tiene {dron.NivelBateria}% bateria");
+            }
+            Console.WriteLine($"La cantidad de drones operativos ahora son: {drones.Count}");
+        }
+    }
+
+    class Cargar
+    {
+        static void Main(string[] args)
+        {
+            CentroControl cc1 = new CentroControl();
+
+            cc1.listarFlota();
+            cc1.removerDronesBajos();
+            cc1.mostrarDronesRestantes();
+
+            Console.ReadKey();
+        }
+    }
+}
